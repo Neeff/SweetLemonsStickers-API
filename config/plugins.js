3c036e26cf44449c9b03ccc8d6f012f76c1eee26
Neeff <dpincheira2016@alu.uct.cl>
2020-09-06 16:10:03 -0300

    add: email provider 'sendgrid'

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,23 @@
-module.exports = ({env}) => ({
-  // ...
-  upload: {
-    provider: 'aws-s3',
-    providerOptions: {
-      accessKeyId: env('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: env('AWS_ACCESS_SECRET'),
-      region: env('AWS_REGION'),
-      params: {
-        Bucket: env('AWS_BUCKET'),
-      },
+module.exports = ({ env }) => ({
+    upload: {
+        provider: 'aws-s3',
+        providerOptions: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET'),
+            region: env('AWS_REGION'),
+            params: {
+                Bucket: env('AWS_BUCKET'),
+            },
+        },
     },
-  },
-  // ...
+    email: {
+        provider: 'sendgrid',
+        providerOptions: {
+            apiKey: env('SENDGRID_API_KEY'),
+        },
+        settings: {
+            defaultFrom: env('EMAIL_STORE'),
+            defaultReplyTo: env('EMAIL_STORE'),
+        },
+    },
 });
