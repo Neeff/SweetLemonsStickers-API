0d6c200305531484d27141744460c1727bb6eef9
Neeff <dpincheira2016@alu.uct.cl>
2020-09-11 03:25:23 -0300

    add: implement route init and response, payment process carried out successfully

--- api/payment/controllers/payment.js
+++ api/payment/controllers/payment.js
@@ -1,22 +1,57 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
-
+const Transbank = require('transbank-sdk');
 module.exports = {
 
     init: async (ctx) => {
-        const { shoppingCart } = ctx.request.body;
-        ctx.send({ shoppingCart });
+        // const {shoppingCart, ammount} = ctx.request.body;
+        //ctx.send({shoppingCart});
+        const amount = 10000;
+        const sessionId = 'user_id';
+        const buyOrder = strapi.services.payment.createUUID();
+        const returnUrl = 'http://localhost:1337/payments/response';
+        const finalUrl = 'http://localhost:1337/payments/finish';
+        const transaction = strapi.services.payment.Webpay();
+        try {
+            const response = await transaction.initTransaction(amount, buyOrder, sessionId, returnUrl, finalUrl);
+            if (response) {
+
+                const { token, url } = response
+                ctx.send({
+                    token,
+                    url,
+                    amount
+                });
+            }
+        } catch (error) {
+            console.log(error);
+            ctx.send({
+                error
+            });
+        }
     },
 
-    response: async (ctx) => {
-        ctx.send({
-            status: "here"
-        })
 
+
+    response: async (ctx) => {
+        const transaction = strapi.services.payment.Webpay();
+        const token = ctx.request.body.token_ws;
+        try {
+            const response = await transaction.getTransactionResult(token);
+            const output = response.detailOutput[0];
+            if (output.responseCode === 0) {
+                // La transacción se ha realizado correctamente
+                ctx.status = 307;
+                ctx.redirect(response.urlRedirection);
+                ctx.body = {
+                    "token_ws": token
+                }
+            }
+            //console.log(response)
+
+        } catch (error) {
+            console.log(error);
+        }
     },
 
     finish: async (ctx) => {
--- api/payment/services/payment.js
+++ api/payment/services/payment.js
@@ -1,18 +1,22 @@
 'use strict';
-
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
- * to customize this service
- */
+const Transbank = require('transbank-sdk');
 
 module.exports = {
     createUUID: (ctx) => {
         let dt = new Date().getTime();
-        let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+        let uuid = 'xxxxx-xxxx-4xxx-yxxx-xxxxx'.replace(/[xy]/g, c => {
             let r = (dt + Math.random() * 16) % 16 | 0;
             dt = Math.floor(dt / 16);
             return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
         });
         return uuid;
+    },
+    Webpay: () => {
+        const transaction = new Transbank
+            .Webpay(Transbank.Configuration
+                .forTestingWebpayPlusNormal())
+            .getNormalTransaction();
+        return transaction;
+
     }
 };
